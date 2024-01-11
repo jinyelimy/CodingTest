c09e95a1e5562a67a44adc3dcbc3071c8d929ab6
jinyelimy <144537803+jinyelimy@users.noreply.github.com>
2024-01-11 23:03:27 +0900

    [level Lv.0] Title: 문자열 출력하기, Time: 199.40 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		String a = scanner.next();
+		System.out.println(a);
+    }
+}
